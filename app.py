612f8671977d5f56a89112b7af4b6591b027dd13
soumya <work.soumyadipadak@gmail.com>
2025-06-16 20:48:45 +0530

    update

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def file_too_large(e):
 
 if __name__ == "__main__":
     # Use the PORT environment variable provided by Render, defaulting to 8000
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
